8c4b3537d943abc16ce0834196c255073eaea957
CasualAnime <Naruto170612@gmail.com>
2025-08-10 22:10:40 -0400

    Final Changes before dropping
    Final things like rotating player based on mouse position and sprites. Planning to drop the game so i can learn more before maybe coming back

--- SoloGameJam/Assets/Scripts/PlayerController.cs
+++ SoloGameJam/Assets/Scripts/PlayerController.cs
@@ -4,19 +4,33 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public Camera cam;
     public float speed;
 
+
     private Vector2 inputVector;
+    private Vector2 mousePos;
     [SerializeField] Rigidbody2D rb;
+    //[SerializeField] Rigidbody2D swordRb;
 
     public void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        //swordRb = GetComponentInChildren<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
     }
 
     private void FixedUpdate()
     {
-        rb.linearVelocity = new Vector2(inputVector.x * speed * Time.deltaTime, inputVector.y * speed * Time.deltaTime);
+        rb.linearVelocity = new Vector2(inputVector.x * speed * Time.deltaTime, inputVector.y * speed * Time.deltaTime); //player movement
+
+        Vector2 lookDir = mousePos - rb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
     }
 
     public void Move(InputAction.CallbackContext context)
@@ -28,8 +42,9 @@ public class PlayerController : MonoBehaviour
     {
         if (context.performed)
         {
-            Debug.Log("Attack" + context.phase);    
+            Debug.Log("Attack" + context.phase);
         }
-        
+
     }
+    
 }
